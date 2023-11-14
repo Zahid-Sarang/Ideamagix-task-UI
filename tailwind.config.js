5123bbecdec335fa605e0c9f8d28278a4dae4afe
zahid-soft <zahidsarang95@gmail.com>
2023-11-14 17:05:22 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"background-color": "#F2EFE5",
+				"primary-text": "#020100",
+				"Active-text": "#56Ab69",
+				secondary: "#6c695f",
+				"secondary-text": "#838078",
+				"card-color": "#E6E3D8",
+			},
+		},
+	},
+	plugins: [],
+};
